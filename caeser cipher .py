14c40293e2a15019991886732d3de9c60b7864a6
simplysweet <nikithasreeca6@gmail.com>
2026-09-25 20:05:23 +0530

    Update caeser cipher .py

--- caeser cipher .py
+++ caeser cipher .py
@@ -18,28 +18,27 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
 print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
 def caesar(original_text,shift_amount,encode_or_decode):
     output_text=""
     if encode_or_decode=="decode":
-            shift_amount*=-1
+        shift_amount*=-1
     for letter in original_text:
         if letter not in alphabet:
             output_text+=letter
         else:             
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position%=len(alphabet)
-        output_text+=alphabet[shifted_position]
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position%=len(alphabet)
+            output_text+=alphabet[shifted_position]
     print(f"The {encode_or_decode}d text is {output_text}")
-caesar(text,shift,direction)
+
 
 should_continue=True
 while should_continue:
-       restart=input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
-       if restart=="no":
-          should_continue=False
-          print("Goodbye")
+      direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+      text = input("Type your message:\n").lower()
+      shift = int(input("Type the shift number:\n"))
+      caesar(text,shift,direction)
+      restart=input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+      if restart=="no":
+         should_continue=False
+         print("Goodbye")
